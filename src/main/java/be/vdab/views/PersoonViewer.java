b43103408fdfe78c5d08d2dfb9b21bb4d6948f40
Maarten Westelinck <maartenwestelinck@hotmail.com>
2017-01-31 10:17:12 +0100

    taak 1.2 afgewerkt

--- src/main/java/be/vdab/views/PersoonViewer.java
+++ src/main/java/be/vdab/views/PersoonViewer.java
@@ -10,10 +10,19 @@ import java.util.List;
  */
 public final class PersoonViewer {
 
+    private boolean aantalKinderenTonen;
+
     public void afbeelden(List<Persoon> personen) {
         for (Persoon p : personen) {
-            System.out.printf("persoonNr: %d, voornaam: %s, familienaam: %s, aantalKinderen: %d\n", p.getPersoonNr(), p.getVoornaam(), p.getFamilienaam(), p.getAantalKinderen());
+            if(aantalKinderenTonen){
+                System.out.printf("persoonNr: %d, voornaam: %s, familienaam: %s, aantalKinderen: %d\n", p.getPersoonNr(), p.getVoornaam(), p.getFamilienaam(), p.getAantalKinderen());
+            } else {
+                System.out.printf("persoonNr: %d, voornaam: %s, familienaam: %s\n", p.getPersoonNr(), p.getVoornaam(), p.getFamilienaam());
+            }
         }
     }
 
+    public void setAantalKinderenTonen(boolean aantalKinderenTonen) {
+        this.aantalKinderenTonen = aantalKinderenTonen;
+    }
 }
